3849bc0617a1df573210ba8cad92e562fb3d355d
Balasankar C <balasankarc@autistici.org>
2015-10-26 22:07:53 +0530

    Imported Debian patch 0.8.0-1

---
+++ debian/hyphen-indic.default.ex
@@ -0,0 +1,10 @@
+# Defaults for hyphen-indic initscript
+# sourced by /etc/init.d/hyphen-indic
+# installed at /etc/default/hyphen-indic by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(hyphen-indic):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="hyphen-indic" command="/usr/bin/hyphen-indic"
