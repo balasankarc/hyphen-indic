4933efc829f93194d65e02aac5bccc124fa8abe1
Balasankar C <balasankarc@autistici.org>
2015-10-26 22:12:39 +0530

    Remove prebuilt examples

--- debian/hyphen-indic.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the hyphen-indic package
-#
-0 4	* * *	root	[ -x /usr/bin/hyphen-indic_maintenance ] && /usr/bin/hyphen-indic_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(hyphen-indic):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="hyphen-indic" command="/usr/bin/hyphen-indic"
